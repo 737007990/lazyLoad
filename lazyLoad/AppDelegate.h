4b88e440ffcd5b420efd42da2ce923fe59bf65b7
737007990 <737007990@qq.com>
2013-07-12 14:14:22 +0800

    Initial Commit

---
+++ lazyLoad/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lazyLoad
+//
+//  Created by zy on 13-7-12.
+//  Copyright (c) 2013年 zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ lazyLoad/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  lazyLoad
+//
+//  Created by zy on 13-7-12.
+//  Copyright (c) 2013年 zy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
